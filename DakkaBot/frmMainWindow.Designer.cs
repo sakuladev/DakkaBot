710a87d8e7984cb24810d6cde68f12c3d45d3087
CCorellian <ccorellian@gmail.com>
2018-02-05 16:35:21 +0100

    Discord.net lib/ref required.

---
+++ DakkaBot/frmMainWindow.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace DakkaBot
+{
+    partial class frmMainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnActivate = new System.Windows.Forms.Button();
+            this.txtLog = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnActivate
+            // 
+            this.btnActivate.Location = new System.Drawing.Point(320, 405);
+            this.btnActivate.Name = "btnActivate";
+            this.btnActivate.Size = new System.Drawing.Size(75, 23);
+            this.btnActivate.TabIndex = 0;
+            this.btnActivate.Text = "button1";
+            this.btnActivate.UseVisualStyleBackColor = true;
+            this.btnActivate.Click += new System.EventHandler(this.btnActivate_Click);
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(12, 12);
+            this.txtLog.Name = "txtLog";
+            this.txtLog.Size = new System.Drawing.Size(731, 387);
+            this.txtLog.TabIndex = 1;
+            this.txtLog.Text = "";
+            // 
+            // frmMainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(755, 440);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.btnActivate);
+            this.Name = "frmMainWindow";
+            this.Text = "Main";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnActivate;
+        private System.Windows.Forms.RichTextBox txtLog;
+    }
+}
+
---
+++ DakkaBot/frmMainWindow.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Discord;
+using Discord.Net;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace DakkaBot
+{
+    public partial class frmMainWindow : Form
+    {
+        public frmMainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public Task Log(LogMessage msg)
+        {
+            txtLog.Text += msg.ToString();
+            return Task.CompletedTask;
+        }
+
+        private DiscordSocketClient _client;
+
+        public async Task Loop()
+        {
+            try
+            {
+                _client = new DiscordSocketClient();
+                _client.Log += Log;
+
+                string token = "NDA5OTc5MzE4NDg0MzM2NjQw.DVoI0Q.Yn-JkxV5KjwTn58FKgyc4xejAUY";
+
+                await _client.LoginAsync(TokenType.Bot, token);
+                await _client.StartAsync();
+
+                await Task.Delay(-1);
+            }
+            catch (Exception e)
+            {
+                txtLog.Text += "Error: " + e.Message;
+
+            }
+        }
+
+        private void btnActivate_Click(object sender, EventArgs e)
+            => new frmMainWindow().Loop().GetAwaiter().GetResult();
+    }
+}
